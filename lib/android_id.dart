85923e18353e8c128b4593c1debd6fca0eaa8041
nohli <43643339+nohli@users.noreply.github.com>
2022-08-06 15:57:32 +0200

    Make variable private, add type to method

--- lib/android_id.dart
+++ lib/android_id.dart
@@ -5,8 +5,8 @@ class AndroidId {
   const AndroidId();
 
   /// The method channel used to interact with the native platform.
-  static const methodChannel = MethodChannel('android_id');
+  static const _methodChannel = MethodChannel('android_id');
 
   /// Calls the native method to retrieve the Android ID.
-  Future<String?> getId() => methodChannel.invokeMethod('getId');
+  Future<String?> getId() => _methodChannel.invokeMethod<String?>('getId');
 }
