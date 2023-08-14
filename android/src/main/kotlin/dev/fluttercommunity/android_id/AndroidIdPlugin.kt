43abd5ccf2ef06258c4a30c35802fc42719c5fe5
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2023-08-14 03:38:23 +0200

    Refactor native code

--- android/src/main/kotlin/dev/fluttercommunity/android_id/AndroidIdPlugin.kt
+++ android/src/main/kotlin/dev/fluttercommunity/android_id/AndroidIdPlugin.kt
@@ -4,23 +4,21 @@ import android.annotation.SuppressLint
 import android.content.ContentResolver
 import android.provider.Settings
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-
 /** AndroidIdPlugin */
-class AndroidIdPlugin: FlutterPlugin, MethodCallHandler {
+class AndroidIdPlugin : FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private lateinit var channel: MethodChannel
 
-  private lateinit var contentResolver : ContentResolver
+  private lateinit var contentResolver: ContentResolver
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     contentResolver = flutterPluginBinding.applicationContext.contentResolver
@@ -29,10 +27,15 @@ class AndroidIdPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getId") {
-      result.success(getAndroidId())
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "getId" -> {
+        try {
+          result.success(getAndroidId())
+        } catch (e: Exception) {
+          result.error("ERROR_GETTING_ID", "Failed to get Android ID", e.localizedMessage)
+        }
+      }
+      else -> result.notImplemented()
     }
   }
 
@@ -40,8 +43,9 @@ class AndroidIdPlugin: FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(null)
   }
 
+  // Fetch the Android ID while suppressing lint warning about hardware IDs
   @SuppressLint("HardwareIds")
   private fun getAndroidId(): String? {
-    return Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID);
+    return Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
   }
 }
